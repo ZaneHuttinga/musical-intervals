4ba45f7018840b2bb98aba8aeb39ddc5ecd073a7
Zane Huttinga <ZaneHuttinga@gmail.com>
2024-07-21 18:18:10 -0500

    Write key parts of basic.py script. The script now poses the question, takes an input and grades the answer.

--- basic.py
+++ basic.py
@@ -1,3 +1,29 @@
 import pandas as pd
 import random
 
+basic = pd.read_csv('musical-intervals/intervals.csv', usecols=[*range(0,7)])
+
+first = list(basic.iloc[0:7,0])
+
+intervals = ['second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
+
+root_note = random.choice(first)
+for i in range(7):
+    if root_note == basic.iloc[i,0]:
+        root_index = i
+
+interval = random.choice(intervals)
+for j in range(6):
+    if interval == intervals[j]:
+        interval_index = j
+
+print(interval_index)
+
+print(f'\nThe root note is {root_note}. What is the {interval}?\n')
+
+x = input()
+
+if x == basic.iloc[root_index,interval_index+1]:
+    print('\nCorrect.\n')
+else:
+    print('\nTry again.\n')
